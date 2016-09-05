ff6610e917c4ecbae9b7efa41715f6066ef30f13
Dan Holland <d.holland@f5.com>
2016-09-05 12:30:28 -0700

    Add project files.

---
+++ MovieCat/MC/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\holland\documents\visual studio 2015\Projects\MovieCat\MovieCat\MC\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
